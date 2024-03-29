6525ecf8a56f4452587000c92353ac4f5bfb3782
AdonChel <coffi1997@inbox.ru>
2024-03-29 19:42:46 +0500

    card and tabbarItem

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, ReactNode, MouseEventHandler } from 'react';
 import bridge, { UserInfo } from '@vkontakte/vk-bridge';
-import { View, ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, SplitLayout, SplitCol } from '@vkontakte/vkui';
+import { ScreenSpinner, AdaptivityProvider, AppRoot, ConfigProvider, Tabbar, TabbarItem, View, Panel, PanelHeader, CardGrid, Card, Spacing, PanelHeaderBack, PanelHeaderButton} from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
+import {Icon28NewsfeedOutline, Icon28UserCircleOutline, Icon28MessageOutline, Icon24MessageOutline, Icon28BillheadOutline, Icon28Square4Outline} from '@vkontakte/icons';
 
 import Home from './panels/Home';
-import Persik from './panels/Persik';
+
 
 const App = () => {
 	const [activePanel, setActivePanel] = useState('home');
@@ -24,18 +25,54 @@ const App = () => {
 		setActivePanel(e.currentTarget.dataset.to ?? 'home');
 	};
 
+	const [simple, setSimple] = useState('one');
+	const [text, setText] = useState('one');
+
+
 	return (
 		<ConfigProvider>
 			<AdaptivityProvider>
 				<AppRoot>
-					<SplitLayout popout={popout}>
-						<SplitCol>
-							<View activePanel={activePanel}>
-								<Home id='home' fetchedUser={fetchedUser} go={go} />
-								<Persik id='persik' go={go} />
-							</View>
-						</SplitCol>
-					</SplitLayout>
+				<View activePanel="card">
+				<Panel id="card">
+				<PanelHeader></PanelHeader>
+					<CardGrid size="l" spaced>
+						<Card>
+							<div style={{ paddingBottom: '30%', backgroundColor: 'white', borderRadius: 20}} />
+						</Card>
+					</CardGrid>
+					<CardGrid size="s" spaced>
+						<Card>
+							<div style={{ paddingBottom: '50%', backgroundColor: 'white', borderRadius: 20, width: '100%'}} />
+						</Card>
+						<Card>
+							<div style={{ paddingBottom: '50%', backgroundColor: 'white', borderRadius: 20, width: '100%'}} />
+						</Card>
+					</CardGrid>
+					<CardGrid size="l" spaced>
+						<Card>
+							<div style={{ paddingBottom: '30%', backgroundColor: 'white', borderRadius: 20}} />
+						</Card>
+					</CardGrid>
+					<Spacing size={16} />
+				</Panel>
+				</View>
+				<div style={{ maxWidth: 768, margin: 'auto' }}>
+					<Tabbar style={{ position: 'static', margin: '10px 0' }}>
+					<TabbarItem selected={text === 'one'} onClick={() => setText('one')} text="Сервисы">
+							<Icon28Square4Outline />
+						</TabbarItem>
+						<TabbarItem selected={text === 'two'} onClick={() => setText('two')} text="Новости">
+							<Icon28NewsfeedOutline />
+						</TabbarItem>
+						<TabbarItem selected={text === 'three'} onClick={() => setText('three')} text="Профиль">
+							<Icon28UserCircleOutline />
+						</TabbarItem>
+						<TabbarItem selected={text === 'four'} onClick={() => setText('four')} text="Мессенджер">
+							<Icon28MessageOutline />
+						</TabbarItem>
+					</Tabbar>
+  				</div>
 				</AppRoot>
 			</AdaptivityProvider>
 		</ConfigProvider>
--- front/src/panels/Home.tsx
+++ front/src/panels/Home.tsx
@@ -1,7 +1,7 @@
 import React, { MouseEventHandler } from 'react';
 import PropTypes from 'prop-types';
 
-import { Panel, PanelHeader, Header, Button, Group, Cell, Div, Avatar } from '@vkontakte/vkui';
+import { Panel, Epic } from '@vkontakte/vkui';
 import { UserInfo } from '@vkontakte/vk-bridge';
 
 interface Props {
@@ -12,24 +12,7 @@ interface Props {
 
 const Home: React.FC<Props> = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
-		<PanelHeader>Example</PanelHeader>
-		{fetchedUser &&
-		<Group header={<Header mode="secondary">User Data Fetched with VK Bridge</Header>}>
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				subtitle={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
-
-		<Group header={<Header mode="secondary">Navigation Example</Header>}>
-			<Div>
-				<Button stretched size="l" mode="secondary" onClick={go} data-to="persik">
-					Show me the Persik, please
-				</Button>
-			</Div>
-		</Group>
+		
 	</Panel>
 );
 
